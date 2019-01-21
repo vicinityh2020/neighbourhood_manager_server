ee35437acae0802516327cda945aa33bf588aa6f
Jorge Almela <jorgealmela@Jorges-MacBook-Pro.local>
2019-01-21 10:32:32 +0100

    FIRST COMMIT of vcnt apicode -- inherits from original vicinity repo

--- vicinityManager/client/app/components/version/version.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('VicinityManagerApp.version', [
-  'VicinityManagerApp.version.interpolate-filter',
-  'VicinityManagerApp.version.version-directive'
-])
-
-.value('version', '0.1');
--- vicinityManager/client/app/env.js
+++
@@ -1,5 +0,0 @@
-this._env = {};
-this._env.apiUrl = "https://vicinity.bavenir.eu:3000";
-this._env.baseHref = "https://vicinity.bavenir.eu";
-this._env.notificationTimeout = 30000;
-// this._env.apiUrl = "http://localhost:3000";
