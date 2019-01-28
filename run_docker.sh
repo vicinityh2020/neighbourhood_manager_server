8f8e413b3ef3756e0e5d8f9dfd1bdc95262b66c7
Jorge Almela <jorgealmela@Jorges-MacBook-Pro.local>
2019-01-28 14:12:31 +0100

    Add new mails to git secret; update script to fix paths and add gti secret reveal

--- run_docker.sh
+++ run_docker.sh
@@ -12,6 +12,7 @@ where:
 PORT=3001
 ENV="development"
 USER="jorge"
+WORKDIR="/home/$USER/vicinity_nm_api/"
 
 # Get configuration
 while getopts 'hd:p:e:u:' option; do
@@ -37,8 +38,10 @@ echo CLEANING...
 docker kill vcnt-app >/dev/null 2>&1
 docker rm vcnt-app >/dev/null 2>&1
 docker rmi vcnt-app >/dev/null 2>&1
+echo REVEAL SECRET FILES
+cd $WORKDIR && git secret reveal
 echo BUILDING THE CONTAINER...
-docker build -f Dockerfile.$ENV -t vcnt-app .
+docker build -f /home/$USER/vicinity_nm_api/Dockerfile.$ENV -t vcnt-app $WORKDIR
 echo SETTING A LOCAL FOLDER FOR THE LOGS...
 mkdir -p /home/$USER/vcnt-logs
 echo RUNNING THE CONTAINER...
@@ -48,7 +51,7 @@ docker run -d -p $PORT:3000 \
         --name vcnt-app \
         --mount type=bind,source=/etc/letsencrypt/live/development.bavenir.eu/privkey.pem,target=/etc/letsencrypt/live/development.bavenir.eu/privkey.pem,readonly \
         --mount type=bind,source=/etc/letsencrypt/live/development.bavenir.eu/fullchain.pem,target=/etc/letsencrypt/live/development.bavenir.eu/fullchain.pem,readonly \
-        --mount type=bind,source=/home/jorge/certificateMongo/ca.pem,target=/home/jorge/certificateMongo/ca.pem,readonly \
+        --mount type=bind,source=/home/$USER/certificateMongo/ca.pem,target=/home/$USER/certificateMongo/ca.pem,readonly \
         -v /home/$USER/vcnt-logs:/app/logs \
         vcnt-app:latest
 # Finish and close
