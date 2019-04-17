f4cb5ba7c47046f40d61da65fa5c4434aea075fc
Jorge Almela <jorgealmela@JorgePc.local>
2019-04-17 14:58:46 +0200

    Update docker with possibility to run with scheduled tasks

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-USAGE="$(basename "$0") [-h -l] [-p port -d domain -n app_name]
+USAGE="$(basename "$0") [-h -l -t] [-p port -d domain -n app_name]
 -- Builds vcnt-app docker on linux/mac
 -- Examples
 ./run.sh -p 3000 -n app-name -d www.example.com
@@ -8,6 +8,8 @@ USAGE="$(basename "$0") [-h -l] [-p port -d domain -n app_name]
 Where:
   Flags:
       -h  shows help
+      -l  local mode
+      -t  adds scheduled tasks (Directory must exist)
   Options with argument:
       -n  <app_name> [ vcnt-app (default) ] [ OBLIGATORY ]
       -p  <port> [ 3000 (default) ] [ OBLIGATORY ]
@@ -18,9 +20,10 @@ Where:
   PORT=3000
   DOMAIN=false
   LOCAL=false
+  SCHEDULED=false
 
   # Get configuration
-  while getopts 'hd:ld:p:d:n:' OPTION; do
+  while getopts 'hd:ld:td:p:d:n:' OPTION; do
     case "$OPTION" in
       h)
         echo "$USAGE"
@@ -37,6 +40,9 @@ Where:
         ;;
       l)
         LOCAL=true
+        ;;
+      t)
+        SCHEDULED=true
         ;;
     esac
   done
@@ -64,7 +70,7 @@ if [ ${LOCAL} == true ]; then
         --name ${NAME} \
         ${NAME}:latest
 
-else
+elif [ ${SCHEDULED} == true ]; then
 
   # logs path (OPTIONAL)
   LOG_DIR=~/docker_data/logs/${NAME}
@@ -74,10 +80,13 @@ else
   KEY=/etc/letsencrypt/live/${DOMAIN}/privkey.pem
   # ssl certificate (OPTIONAL)
   CERT=/etc/letsencrypt/live/${DOMAIN}/fullchain.pem
+  # Workdir
+  WORKDIR=~/vicinity_scheduled_tasks
 
   # Remove 1st and 2nd --mount if no SSL
   # Remove 3rd --mount if no certificate used with mongo
   # Remove -v if you don't want to have the logs in your local machine
+
   docker run -d -p $PORT:3000 \
           -it \
           --rm \
@@ -86,9 +95,38 @@ else
           --mount type=bind,source=${CERT},target=/etc/letsencrypt/fullchain.pem,readonly \
           --mount type=bind,source=${MONGO_CERT}mongoCA.crt,target=/mongo/ssl/mongoCA.crt,readonly \
           --mount type=bind,source=${MONGO_CERT}mongo.pem,target=/mongo/ssl/mongo.pem,readonly \
+          --mount type=bind,source=${WORKDIR}/statistics/statistics.log,target=/opt/vicinity_services/getStatistics/statistics.log,readonly \
+          --mount type=bind,source=${WORKDIR}/annotations/annotations.json,target=/opt/vicinity_services/getAnnotations/annotations.json,readonly \
           -v ${LOG_DIR}:/app/logs \
           ${NAME}:latest
 
           echo "Success! Logs stored under ${LOG_DIR}"
 
+    else
+
+      # logs path (OPTIONAL)
+      LOG_DIR=~/docker_data/logs/${NAME}
+      # mongo path to the certificate (OPTIONAL)
+      MONGO_CERT=~/certificateMongo/
+      # ssl private key (OPTIONAL)
+      KEY=/etc/letsencrypt/live/${DOMAIN}/privkey.pem
+      # ssl certificate (OPTIONAL)
+      CERT=/etc/letsencrypt/live/${DOMAIN}/fullchain.pem
+
+      # Remove 1st and 2nd --mount if no SSL
+      # Remove 3rd --mount if no certificate used with mongo
+      # Remove -v if you don't want to have the logs in your local machine
+
+      docker run -d -p $PORT:3000 \
+              -it \
+              --rm \
+              --name ${NAME} \
+              --mount type=bind,source=${KEY},target=/etc/letsencrypt/privkey.pem,readonly \
+              --mount type=bind,source=${CERT},target=/etc/letsencrypt/fullchain.pem,readonly \
+              --mount type=bind,source=${MONGO_CERT}mongoCA.crt,target=/mongo/ssl/mongoCA.crt,readonly \
+              --mount type=bind,source=${MONGO_CERT}mongo.pem,target=/mongo/ssl/mongo.pem,readonly \
+              -v ${LOG_DIR}:/app/logs \
+              ${NAME}:latest
+
+              echo "Success! Logs stored under ${LOG_DIR}"
 fi
